0412068d65481d540884493f946f4fb43cb286cc
Aniketh <techrunstheworld@gmail.com>
2024-10-01 08:15:29 +0530

    fix:description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Page() {
                 delay={BLUR_FADE_DELAY}
                 className="text-2xl font-bold tracking-tighter sm:text-5xl xl:text-4xl/none"
                 yOffset={8}
-                text={`Hi, I'm ${DATA.name.split(" ")[0]} 🦀`}
+                text={`Hi, I'm ${DATA.name.split(" ")[0]} 🗿`}
               />
               <BlurFadeText
                 className="max-w-[600px] md:text-xl"
